ab458bc87d2b3df3f8a189eced945d564b36066d
amar62059 <amrksop@gmail.com>
2026-06-14 23:19:28 +0530

    Add JwtConstant and update config

--- src/main/java/com/amar/treading/config/AppConfig.java
+++ src/main/java/com/amar/treading/config/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
-    http.sessionManagement (managment -> managemert )
+    http.sessionManagement (managment -> managemert.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+            .authorizeHttpReqests(Authorizae->Authorizae.requstMatchers())
 }
 }
---
+++ src/main/java/com/amar/treading/config/JwtConstant.java
@@ -0,0 +1,5 @@
+package com.amar.treading.config;
+
+public class JwtConstant {
+
+}
